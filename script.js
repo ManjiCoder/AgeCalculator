2327e46fd38f60bcb07906214578fe401fbb1d2f
Manji <manjicoder@gmail.com>
2021-08-24 15:37:18 +0530

    Calculator done

--- script.js
+++ script.js
@@ -1,69 +1,42 @@
-let DOB = document.getElementById("dob");
-let Today = document.getElementById("today");
-let btn = document.getElementById("btn");
-let h2 = document.querySelector('h4');
+// Variables Declartion 
+let a = document.getElementById('dob');
+let b = document.getElementById('desiredDate');
+let btn = document.getElementById('btn')
+let h2 = document.querySelector('h2');
+let h3 = document.querySelector('h3');
+let h4 = document.querySelector('h4');
+let h5 = document.querySelector('h5');
+let weeks = document.getElementById('weeks')
+let hours = document.getElementById('hrs')
+let mins = document.getElementById('mins')
 
-function age() {
+// Funcution
+function calculateAge() {
+    let date1 = a.value;
+    let date2 = b.value;
+    let DOB = new Date(date1);
+    let PickDate = new Date(date2);
 
-    // Variables Declaration
-    let a = DOB.value;
-    let b = Today.value;
-    let c = new Date(a);
-    let d = new Date(b);
-    let year1 = c.getFullYear();
-    let year2 = d.getFullYear();
-    let month1 = c.getMonth();
-    let month2 = d.getMonth();
-    let date1 = c.getDate();
-    let date2 = d.getDate();
-
-    // let fullArr = [c.getDate(), d.getDate(), c.getMonth(), d.getMonth(), c.getFullYear(), d.getFullYear()]
-    // console.log(fullArr);
-    // DO NOT DELETE//
-    //  let dateArr = [c.getFullYear(), d.getFullYear(), c.getMonth(), d.getMonth(), c.getDay(), d.getDay()]; // getDay give return value in Integer from 0-6 0:Sunday, 1:Monday & so on;
-    // console.log(dateArr);
-    // Conditions
-    if (a != b) {
-        let diff = d - c;
-        console.log(diff);
-
-        if (b > a && (date1 == date2) && (month1 == month2)) {
-            let diffYears = year2 - year1;
-            // let minusOne = diffYears - 1;
-            let result = (h2.innerText = `You Are ${diffYears} Years`);
-            console.log("only year is different");
-            console.log(result);
-        }
-        
-        if (b > a && (date1 != date2) || (month1 != month2)) {
-            let diffYear = year2 - year1;
-            let minusOne = diffYear - 1;
-            let result1 = (h2.innerText = `You Are ${minusOne} Years`);
-            console.log("date is different");
-            console.log(result1);
-        }
-    }
-
-    else if (a == b) {
-        alert("Both Dates Are Same")
-    }
-
-    else {
-        alert("Incorrect");
+    if (date1 > date2) {
+        alert("Enter A Valid Date");
+    } else if (date1 == date2) {
+        alert("Both Dates Are Same!");
+    } if (DOB != PickDate) {
+        let difference = PickDate - DOB;
+        let yr = (h2.innerText = Math.round(difference / (1000 * 3600 * 24 * 365.25) * 1000) / 1000 + " Years");
+        let month = (h3.innerText = Math.round(difference / (1000 * 3600 * 24 * 30.417) * 100) / 100 + " Months");
+        let day = (h4.innerText = (difference / 86400000) + " Days");
+        let week = (weeks.innerText = Math.round(difference / (1000 * 3600 * 24 * 7) * 100) / 100 + " Weeks");
+        let hrs = (hours.innerText = (difference / (1000 * 3600)) + " Hours");
+        let minute = (mins.innerText = (difference / (1000 * 60)) + " Minutes");
+        // console.log(difference + " is diff");
+        // console.log(yr + " in Years");
+        // console.log(month + " in months");
+        // console.log(day + " in days");
+        // console.log(week + " in Weeks");
+        // console.log(hrs + " in hrs");
+        // console.log(minute + " in Minutes");
+    } else {
+        alert("Invalid");
     }
 }
-     // if (a > b) {
-            //     alert("Enter A Valid Date")
-            // }
- // console.log(result);
-            // let result = (h2.innerText = diffYear);
-            // console.log(h2.innerText = diffYears + " is your running year");
-            // console.log(a);
-            // console.log(b);
-            // console.log(c);
-            // console.log(d);
-            // console.log(year1);
-            // console.log(year2);
-            // console.log(month1);
-            // console.log(month2);
-
