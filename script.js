4a51ca7e8bc365d9c7449509fdeac00ed9f7e467
Manji <manjicoder@gmail.com>
2022-01-19 22:07:23 +0530

    Bg changed & Nav is removed comments added

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-// Variables Declartion 
-let a = document.getElementById('dob');
-let b = document.getElementById('desiredDate');
-let btn = document.getElementById('btn')
-let yrs = document.getElementById('yrs')
-let months = document.getElementById('months')
-let days = document.getElementById('days')
+// Initializing Variables 
+let a = document.getElementById('dob');         //  To get Input Date of dob
+let b = document.getElementById('desiredDate'); //  To get Input Date of desiredDate
+let btn = document.getElementById('btn');
+let yrs = document.getElementById('yrs');
+let months = document.getElementById('months');
+let days = document.getElementById('days');
 let weeks = document.getElementById('weeks');
 let hours = document.getElementById('hrs');
 let mins = document.getElementById('mins');
 let err = document.getElementById('error'); // This is to show error made by user
 
-// Funcution
+// Function
 function calculateAge() {
-  let date1 = a.value;
-  let date2 = b.value;
-  let DOB = new Date(date1);
-  let PickDate = new Date(date2);
+  let date1 = a.value;            // value of dob
+  let date2 = b.value;            // value desiredDate
+  let DOB = new Date(date1);      // Putting dob value into DOB
+  let PickDate = new Date(date2); // Putting desiredDate value into PickDate
 
   // If-Else Conditions
+
+  // For Invalid Date
   if (date1 > date2) {
     alert("Enter A Valid Date");
     err.style.display = "block";
@@ -30,6 +32,7 @@ function calculateAge() {
     mins.innerHTML = "";
   }
 
+  // For Same Date
   else if (date1 == date2) {
     alert("Both Dates Are Same!");
     err.style.display = "block";
@@ -42,8 +45,11 @@ function calculateAge() {
     mins.innerHTML = "";
   }
 
+  // To get result
   else if (DOB < PickDate) {
-    let difference = PickDate - DOB;
+    let difference = PickDate - DOB;  //  This is Difference in Miliseconds
+
+    // Converting Miliseconds to Years, Months, Days, Weeks, Hours, Minutes, Seconds
     let yr = (yrs.innerText = Math.round(difference / (1000 * 3600 * 24 * 365.25) * 1000) / 1000 + " Years");
     let month = (months.innerText = Math.round(difference / (1000 * 3600 * 24 * 30.417) * 100) / 100 + " Months");
     let day = (days.innerText = (difference / 86400000) + " Days");
@@ -51,34 +57,9 @@ function calculateAge() {
     let hrs = (hours.innerText = (difference / (1000 * 3600)) + " Hours");
     let minute = (mins.innerText = (difference / (1000 * 60)) + " Minutes");
     err.style.display = "none";
-    // console.log(difference + " is diff");
-    // console.log(yr + " in Years");
-    // console.log(month + " in months");
-    // console.log(day + " in days");
-    // console.log(week + " in Weeks");
-    // console.log(hrs + " in hrs");
-    // console.log(minute + " in Minutes");
   }
 
   else {
     alert("Invalid");
-  }
-}
-
-// Navigation
-let hide = document.getElementById('icon');
-let nav = document.getElementById('navbar');
-function toggle_Width() {
-  // setTimeout(function () { nav.classList.toggle("Nav"); }, 100);
-  // console.log(nav);
-  nav.classList.toggle("Nav");
-  if (nav.style.display != "block") {
-    nav.style.display = "block";
-    // nav.style.transition - "all 2s";
-    console.log('block');
-  }
-  else {
-    nav.style.display = "none";
-    console.log('none');
   }
 }
